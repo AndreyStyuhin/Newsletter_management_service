23daaf2e5b692facb246855b0b56d4ac604a9e71
AndreyStyuhin <andreyst@olovyannaya.ru>
2025-10-02 22:53:22 +0900

    mailing users 3

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.AutoField'  # Change this line
     name = 'users'
