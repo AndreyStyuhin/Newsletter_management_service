caadb98b6d562cf80fea5e35f801616c051a4e4d
AndreyStyuhin <andreyst@olovyannaya.ru>
2025-10-02 22:43:04 +0900

    mailing users 2

--- users/views.py
+++ users/views.py
@@ -2,7 +2,23 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
 
+User = get_user_model()
+
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ProfileView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
 
 class LogoutView(APIView):
     permission_classes = [permissions.IsAuthenticated]
