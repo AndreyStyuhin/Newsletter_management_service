3b8599017dd80525788a2af7e3547be293ed2c93
AndreyStyuhin <andreyst@olovyannaya.ru>
2025-10-02 22:37:17 +0900

    mailing users 1

--- core/settings.py
+++ core/settings.py
@@ -34,8 +34,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "users",
     "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",
+    "users",
     "mailings",
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,19 +1,17 @@
-from rest_framework import generics, permissions
-from django.contrib.auth import get_user_model
-from .serializers import UserSerializer
-
-User = get_user_model()
-
-
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [permissions.AllowAny]
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
 
 
-class ProfileView(generics.RetrieveUpdateAPIView):
-    serializer_class = UserSerializer
+class LogoutView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_object(self):
-        return self.request.user
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()  # пометить как недействительный
+            return Response({"detail": "Успешный выход"}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response({"detail": "Неверный токен"}, status=status.HTTP_400_BAD_REQUEST)
